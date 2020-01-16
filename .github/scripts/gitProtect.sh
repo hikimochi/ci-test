d74b2b44be26209938baded2610d60eee7eafd50
Rikiya Hikimochi <r.hikimochi@RikiyanoMacBook-puro.local>
2020-01-16 12:44:08 +0900

    Fix bug.

--- .github/scripts/gitProtect.sh
+++ .github/scripts/gitProtect.sh
@@ -1,5 +1,5 @@
 echo "$GITHUB_REPOSITORY"
 curl -s -H "Accept: application/vnd.github.luke-cage-preview+json" \
   -H "Authorization: token ${GITHUB_API_TOKEN}" \
-  https://api.github.com/repos"$GITHUB_REPOSITORY"/branches/"$TARGET_BRANCH"/protection \
+  https://api.github.com/repos/"$GITHUB_REPOSITORY"/branches/"$TARGET_BRANCH"/protection \
   -X PUT -d @"$JSON"
